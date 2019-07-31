a1d1b45728a56f16d1bd05130eed092e0a0487f0
adnansabbir <adnan.sabbir2012@hmail.com>
2019-07-31 21:14:13 +0600

    Updates

---
+++ precache-manifest.72b15c18478d25e2ae451fc14b0d394d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a773884c67090e3ce3f5b2a9687acc55",
+    "url": "/user_registration_react_app/index.html"
+  },
+  {
+    "revision": "29bb9269370d986f343e",
+    "url": "/user_registration_react_app/static/css/main.9fdfa47d.chunk.css"
+  },
+  {
+    "revision": "2be2b3b38c93bf377727",
+    "url": "/user_registration_react_app/static/js/2.88829aa2.chunk.js"
+  },
+  {
+    "revision": "29bb9269370d986f343e",
+    "url": "/user_registration_react_app/static/js/main.eadbe31f.chunk.js"
+  },
+  {
+    "revision": "d0d0cd8ae38b62571277",
+    "url": "/user_registration_react_app/static/js/runtime~main.3cb1e7c0.js"
+  }
+]);
